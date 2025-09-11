238e091019de7d20ea100ca53963fb5dc0e89f21
Patrick Schmidt <pschmidt@pasit.nl>
2025-09-11 09:52:24 +0200

    day2

---
+++ Live/Infrastructuur/Logging/MyClass.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Logging;
+
+internal class MyClass
+{
+    private ILogger<MyClass> _logger;
+
+    public MyClass(ILogger<MyClass> logger)
+    {
+        _logger = logger;
+    }
+
+    public void DoeIets()
+    {
+        _logger.LogCritical("Kritiek");
+        _logger.LogError("Fout!");
+        _logger.LogWarning("Waarschuwing");
+        _logger.LogInformation("Info");
+        _logger.LogDebug("Debugger");
+        _logger.LogTrace("Trees");
+    }
+}
---
+++ Live/Infrastructuur/Logging/Program.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+
+namespace Logging;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var builder = new ConfigurationBuilder();
+        builder.SetBasePath(Environment.CurrentDirectory);
+        builder.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+        var config2 = builder.Build();
+      
+        var factory = LoggerFactory.Create(config => {
+            // In code:
+            //config.AddFilter((cat, lvl) =>
+            //{
+            //    return cat == "MyCat" && lvl >= LogLevel.Trace;
+            //});
+            // In config:
+            config.AddConfiguration(config2.GetSection("Blaat"));
+
+            //config.ClearProviders();
+            // From package: Microsoft.Extensions.Logging.Console
+            config.AddConsole();
+            config.AddDebug();
+            config.AddEventLog();
+        });
+
+        ILogger logger = factory.CreateLogger("MyCat");
+
+        //logger.BeginScope()
+        logger.LogCritical("Kritiek");
+        logger.LogError("Fout!");
+        logger.LogWarning("Waarschuwing");
+        logger.LogInformation("Info");
+        logger.LogDebug("Debugger");
+        logger.LogTrace("Trees");
+
+        Debug.WriteLine("In debug mode");
+
+        ILogger<MyClass> logger2 = factory.CreateLogger<MyClass>();
+        var c = new MyClass(logger2);
+
+        c.DoeIets();
+        Console.ReadLine();
+    }
+}
